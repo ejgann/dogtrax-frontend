e0e4292ea99c2072fa07b4ccdcac46244c1d4e9b
Emily Gann <aspentree79@gmail.com>
2021-04-25 19:58:09 -0400

    installed packages and imported files to index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 class App extends React.Component {
+  
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/dogs')
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
+  
   render() {
+
     return (
       <div className="App">
         <h1>DogTrax</h1>
