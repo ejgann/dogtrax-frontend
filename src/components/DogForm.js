b66d849248fd14498b9b49863db7f96c11583373
Emily Gann <aspentree79@gmail.com>
2021-05-02 17:27:23 -0400

    building dog input form

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -6,7 +6,16 @@ class DogForm extends React.Component {
     render() {
         return (
             <div>
-                DogForm
+                <form>
+                    <label>Dog's Name:</label> <input type='text' placeholder='Name'/><br></br>
+                    <label>Dog's Age:</label> <input type='text' placeholder='Age'/><br></br>
+                    <label>Dog's Gender:</label> <select name='gender' id='gender'>
+                        <option value='male'>Male</option>
+                        <option value='female'>Female</option>
+                        </select><br></br>
+                    <label>Dog's Breed:</label> <input type='text' placeholder='Breed'></input><br></br>
+                    <label>Special Notes:</label> <textarea placeholder='Notes'></textarea><br></br>
+                </form>
             </div>
         )
     }
