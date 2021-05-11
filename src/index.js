f528b8817b376a4de51963c205d30dae85bccd59
Emily Gann <aspentree79@gmail.com>
2021-05-11 14:23:32 -0400

    imported react router dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
 import dogReducer from './reducers/dogReducer';
 
 import App from './App';
@@ -14,7 +15,9 @@ let store = createStore(dogReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App /> 
+    <Router>
+      <App /> 
+    </Router>
   </Provider>
     , document.getElementById('root')
 );
